e410507ced253fb60589f0bf344ca9b1ad543b62
Yassin <yassin.bahloul@gmx.de>
2016-03-07 19:20:48 +0100

    11727 revised

--- 11727/11727.cpp
+++ 11727/11727.cpp
@@ -1,5 +1,5 @@
 #include <cstdio>
-
+#include <algorithm>
 
 using namespace std;
 
@@ -15,35 +15,10 @@ int main()
 	for(int t = 1; t <= T; ++t)
 	{
 		scanf("%d %d %d", &x[0], &x[1], &x[2]);
-		int res;
 
-		if(x[0] > x[1])
-		{
-			if(x[0] > x[2])
-			{
-				res = x[1] < x[2] ? x[2] : x[1];
-			}
-			else
-			{
-				res = x[0];
-			}
-		}
-		else if(x[0] < x[1])
-		{
-			if(x[0] < x[2])
-			{
-				res = x[1] > x[2] ? x[2] : x[1];
-			}
-			else
-			{
-				res = x[0];
-			}
-		}
-		else
-			res = x[0];
+		sort(x, x+3);
 
-
-		printf("Case %d: %d\n", t, res);
+		printf("Case %d: %d\n", t, x[1]);
 
 	}
 }
