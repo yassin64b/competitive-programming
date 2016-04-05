ace79bf655252fd3ecf7d54961eb705ff5dcabdd
yassin64b <yassin.bahloul@gmx.de>
2016-04-05 19:59:06 +0200

    p12468 shortened

--- chapter1/12468.cpp
+++ chapter1/12468.cpp
@@ -10,7 +10,7 @@ int main()
 
 	while(scanf("%d %d", &a, &b) != EOF && (a != -1 || b != -1))
 	{
-		printf("%d\n", std::min({std::abs(b-a), a+100-b, 100-a+b}));
+		printf("%d\n", std::min({std::abs(b-a), 100-std::abs(b-a)}));
 	}
 	return 0;
 }
