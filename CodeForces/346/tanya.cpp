fa382bdb1e57ffc16172bf73248813d2611a878a
yassin64b <yassin.bahloul@gmx.de>
2016-04-13 21:20:27 +0200

    solved #346 C

---
+++ CodeForces/346/tanya.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define INF 1e9+1
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> v(N);
+	for(int i = 0; i < N; ++i)
+		cin >> v[i];
+	v.push_back(INF);
+
+	sort(v.begin(), v.end());
+
+	int i = 1, sum = 0, j = 0, cnt = 0;
+	vector<int> t;
+
+	for(int j = 0; j <= N; ++j)
+	{
+		if(i == v[j]) ++i;
+		else
+		{
+			while(sum+i <= M && i < v[j])
+			{
+				t.push_back(i);
+				sum += i;
+				++i, ++cnt;
+			}
+			++i;
+		}
+		
+	}
+
+	cout << cnt << endl;
+	for(int toy: t)
+		cout << toy << " ";
+
+	return 0;
+}
