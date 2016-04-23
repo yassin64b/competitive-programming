c49e4a5fdd590ab0c3a19adb7ae17125c1958835
yassin64b <yassin.bahloul@gmx.de>
2016-04-23 20:26:05 +0200

    avoid roads solved

--- SRM/practice/avoidRoadsLessMemory.cpp
+++ SRM/practice/avoidRoadsLessMemory.cpp
@@ -5,7 +5,7 @@
 #include <set>
 #include <utility>
 #include <sstream>
-
+#include <map>
 using namespace std;
 
 typedef long long lli;
@@ -17,7 +17,7 @@ public:
 	lli numWays(int width, int height, vector<string> bad)
 	{
 		lli dp[101][101] = {0}; 
-		bool inv[101][101] = {0};
+		map<pii, set<pii>> m;
 
 		int a, b, c, d;
 		for(string& s: bad)
@@ -25,18 +25,21 @@ public:
 			stringstream ss(s);
 			ss >> a >> b >> c >> d;
 
-			inv[a][b] = true, inv[c][d] = true;
+			if(a < c || b < d)
+				m[make_pair(a,b)].insert(make_pair(c,d));
+			else
+				m[make_pair(c,d)].insert(make_pair(a,b));
 		}
 
 		dp[0][0] = 1;
 		for(int i = 1; i < width+1; ++i)
 		{
-			if(inv[i][0] && inv[i-1][0]) break;
+			if(m[make_pair(i-1,0)].find(make_pair(i,0)) != m[make_pair(i-1,0)].end()) break;
 			dp[i][0] = 1;
 		}
-		for(int j = 1; j < width+1; ++j)
+		for(int j = 1; j < height+1; ++j)
 		{
-			if(inv[0][j] && inv[0][j-1]) break;
+			if(m[make_pair(0,j-1)].find(make_pair(0,j)) != m[make_pair(0,j-1)].end()) break;
 			dp[0][j] = 1;
 		}
 
@@ -44,9 +47,9 @@ public:
 		{
 			for(int j = 1; j < height+1; ++j)
 			{	
-				if(!inv[i][j] || !inv[i-1][j])
+				if(m[make_pair(i-1,j)].find(make_pair(i,j)) == m[make_pair(i-1,j)].end())
 					dp[i][j] += dp[i-1][j];
-				if(!inv[i][j] || !inv[i][j-1])
+				if(m[make_pair(i,j-1)].find(make_pair(i,j)) == m[make_pair(i,j-1)].end())
 					dp[i][j] += dp[i][j-1];
 			}
 		}
@@ -54,7 +57,7 @@ public:
 		/*for(int i = 0; i < width+1; ++i)
 		{
 			for(int j = 0; j < height+1; ++j)
-				cout << dp[i][j][0] << " ";
+				cout << dp[i][j] << " ";
 			cout << endl;
 		}*/
 
@@ -69,7 +72,7 @@ public:
 	cin >> w >> h;
 
 	AvoidRoads av;
-	cout << av.numWays(w, h, {}) << endl;
+	cout << av.numWays(w, h, {"1 2 2 2", "1 1 2 1"}) << endl;
 	
 	return 0;
 }*/
