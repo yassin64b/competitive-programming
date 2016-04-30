c09838cf3d1ed7250c4bf4eaa907116dca49db5a
yassin64b <yassin.bahloul@gmx.de>
2016-04-30 20:35:58 +0200

    solved B (small only) and A

---
+++ GCJ/2016/1B/A.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<pair<int,int>> vii;
+typedef pair<int,int> pii;
+
+bool finished(vector<int>& v)
+{
+	for(int i = 0; i < (int)v.size(); ++i)
+		if(v[i] != 0) return false;
+	return true;
+}
+
+bool dod(vector<int>& cnt, string s)
+{
+	bool flag = true;
+	vector<int> tmp(cnt);
+
+	for(int i = 0; flag && i < (int)s.size(); ++i)
+	{
+		if(!tmp[s[i]-'A'])
+			flag = false;
+
+		--tmp[s[i]-'A'];
+	}
+
+	if(!flag) return false;
+
+	for(int i = 0; flag && i < (int)s.size(); ++i)
+	{
+		--cnt[s[i]-'A'];
+	}
+
+	return true;
+}	
+
+int main()
+{
+	int T; 
+	cin >> T;
+
+	for(int t = 1; t <= T; ++t)
+	{	
+		string S;
+		cin >> S;
+
+		vector<int> cnt(26, 0);
+		for(int i = 0; i < (int)S.size(); ++i)
+			++cnt[S[i]-'A'];
+
+		vector<int> num;
+
+		while(!finished(cnt))
+		{
+			while(dod(cnt, "ZERO")) //unique Z
+				num.push_back(0);
+			while(dod(cnt, "SIX")) //unique X
+				num.push_back(6);
+			while(dod(cnt, "FOUR")) // U
+				num.push_back(4);
+			while(dod(cnt, "FIVE")) //rest F
+				num.push_back(5);
+			while(dod(cnt, "TWO")) //W
+				num.push_back(2);
+			while(dod(cnt, "EIGHT")) //G
+				num.push_back(8);
+			while(dod(cnt, "SEVEN")) //rest V
+				num.push_back(7);
+			while(dod(cnt, "ONE")) //rest O
+				num.push_back(1);
+			while(dod(cnt, "THREE")) //rest R
+				num.push_back(3);
+			while(dod(cnt, "NINE")) //rest
+				num.push_back(9);
+		}
+
+		sort(num.begin(), num.end());
+
+
+		cout << "Case #" << t << ": ";
+		for(int& x: num)
+			cout << x;
+		cout << endl;
+	}
+
+	return 0;
+}
+
+
---
+++ GCJ/2016/1B/Bbrute.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <cctype>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<pair<int,int>> vii;
+typedef pair<int,int> pii;
+
+void solve(string& sc, string& sj)
+{
+	string resc, resj;
+	int dist = 2000000000;
+	int limit = 10, test = 1;
+	while(++test <= sc.size())
+		limit *= 10;
+
+	for(int i = 0; i < limit; ++i)
+	{
+		string s1 = to_string(i);
+
+		while(s1.size() < sc.size())
+			s1 = '0' + s1;
+
+		for(int j = 0; j < limit; ++j)
+		{
+			string s2 = to_string(j);
+			while(s2.size() < sc.size())
+				s2 = '0' + s2;
+
+			bool isMatch = true;
+
+			for(int k = 0; isMatch && k < (int)sc.size(); ++k)
+			{
+				if(sc[k] != '?' && sc[k] != s1[k]) isMatch = false;
+				if(sj[k] != '?' && sj[k] != s2[k]) isMatch = false;
+			}
+
+			if(isMatch)
+			{
+				int tmp = (i < j ? j-i : i-j);
+				if(tmp < dist)
+				{
+					dist = tmp;
+					resc = s1, resj = s2;
+				}
+				
+			}
+		}
+	}
+	sc = resc;
+	sj = resj;
+}
+
+
+int main()
+{
+	int T; 
+	cin >> T;
+
+	for(int t = 1; t <= T; ++t)
+	{	
+		string C, J, resc1, resc2, resj1, resj2;	
+		cin >> C >> J;
+
+		solve(C, J);
+
+		cout << "Case #" << t << ": " << C << " " << J << endl;
+
+	}
+
+}
