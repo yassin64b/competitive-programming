baaaf337600b7ba37ef0688a1b7f11a111159126
yassin64b <yassin.bahloul@gmx.de>
2016-05-14 16:00:07 +0200

    removed wrong format strings and typecasts from p100

--- UVa/00100rec.cpp
+++ UVa/00100rec.cpp
@@ -16,23 +16,23 @@ int single(int n)
 	if (n == 1)
 		return 1;
 
-	if (n < SIZE && val[(int)n] != 0)
-		return val[(int)n];
+	if (n < SIZE && val[n] != 0)
+		return val[n];
 
 	int res = 1 + single(((n % 2 == 0) ? (n / 2) : (3 * n + 1)));
 
 	if (n < SIZE)
-		val[(int)n] = res;
+		val[n] = res;
 
 	return res;
 }
 
 int main()
 {
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 	int i, j, maxv, low, up;
 
-	while (scanf("%lld %lld", &i, &j) != EOF)
+	while (scanf("%d %d", &i, &j) != EOF)
 	{
 		maxv = 0;
 
@@ -44,7 +44,7 @@ int main()
 		for (int t = low; t <= up; ++t)
 			maxv = std::max(maxv, single(t));
 
-		printf("%lld %lld %lld\n", i, j, maxv);
+		printf("%d %d %d\n", i, j, maxv);
 	}
 	return 0;
 }
