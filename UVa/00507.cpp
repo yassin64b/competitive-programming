76828fa8b87f80bb20e42505f41ed802ad382f34
yassin64b <yassin.bahloul@gmx.de>
2016-05-19 00:31:07 +0200

    solved p507 (dp)

--- UVa/00507.cpp
+++ UVa/00507.cpp
@@ -6,45 +6,27 @@ using namespace std;
 
 int main()
 {
-	int b;
-	cin >> b;
+	int B;
+	cin >> B;
 
-	for(int i = 0; i < b; ++i)
+	for(int b = 0; b < B; ++b)
 	{
-		int s; 
+		int s, sum = 0, val, opt = 0, ri = 0, rj = 0, j = 0;
 		cin >> s;
-		vector<int> v(s-1);
+
 		for(int i = 0; i < s-1; ++i)
-			cin >> v[i];
-
-		vector<int> dp(s, 0);
-		int maxv = numeric_limits<int>::min();
-		for(int i = 1; i < s; ++i)
 		{
-			dp[i] = max((dp[i-1]==-1 ? 0 : dp[i-1])+v[i-1], -1);
-			maxv = max(maxv, dp[i]);
-		}
+			cin >> val;
+			sum += val;
 
-		if(maxv < 0)
-		{
-			cout << "Route " << i+1 << " has no nice parts" << endl;
-			continue;
-		}	
-
-		int ri = 0, rj = 0, cnt = 0;
-		for(int i = s-1; i >= 1; --i)
-		{	
-			if(dp[i] != maxv) continue;
-			int j = i;
-			cnt = 0;
-			while(i >= 1 && dp[i] >= 0)
-				--i, ++cnt;
-			
-			//cout << "i, j: " << i << '\t' << j << endl;
-			if(j-i >= rj-ri)
-				rj = j, ri = i;
+			if(sum > opt || (sum == opt && i+1 - j > ri - rj))
+				opt = sum, rj = j, ri = i+1;
+			if(sum < 0)
+				sum = 0, j = i+1;
 		}
-		
-		cout << "The nicest part of route " << i+1 << " is between stops " << ri+1 << " and " << rj+1 << endl;			
+		if(opt > 0)
+			cout << "The nicest part of route " << b+1 << " is between stops " << rj+1 << " and " << ri+1 << endl;
+		else
+			cout << "Route " << b+1 << " has no nice parts" << endl;			
 	}
 }
