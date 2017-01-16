fe1bb67a6a0e6698314573dce1b69ed09f1dfdc1
yassin64b <yassin.bahloul@gmx.de>
2017-01-16 19:00:00 +0100

    Library added FenwickTree and UnionFind data structures

---
+++ LIB/FenwickTree.cc
@@ -0,0 +1,28 @@
+inline int64_t LSOne(int64_t b) {
+    return (b & (-b));
+}
+
+class FenwickTree {
+private:
+    vector<int64_t> ft;
+public:
+    FenwickTree(int64_t n) {
+        ft.assign(n + 1, 0);
+    }
+    int64_t rsq(int64_t b) {
+        int sum = 0;
+        for (; b; b -= LSOne(b)) {
+            sum += ft[b];
+        }
+        return sum;
+    }
+    int64_t rsq(int64_t a, int64_t b) {
+        if (a > b) return 0;
+        return rsq(b) - (a == 1 ? 0 : rsq(a - 1));
+    }
+    void adjust(int64_t k, int64_t v) {
+        for ( ; k < (int)ft.size(); k += LSOne(k)) {
+            ft[k] += v;
+        }
+    }
+};
