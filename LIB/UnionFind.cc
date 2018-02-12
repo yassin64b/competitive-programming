c68a1f891be67fbc35048cb51a86d9f97d15dad4
yassin64b <yassin.bahloul@gmx.de>
2018-02-12 13:29:44 +0100

    LIB modified UnionFind to keep track of set sizes

--- LIB/UnionFind.cc
+++ LIB/UnionFind.cc
@@ -1,37 +1,44 @@
 class UnionFind {
 private:
-    vector<int> p, rank;
-    int numSets;
+    vector<int> p, rank, sz;
+    int num_sets;
 public:
     UnionFind(int N) {
         rank.assign(N, 0);
         p.assign(N, 0);
+        sz.assign(N, 1);
         for (int i = 0; i < N; ++i) {
             p[i] = i;
         }
-        numSets = N;
+        num_sets = N;
     }
-    int findSet(int i) {
-        return (p[i] == i) ? i : (p[i] = findSet(p[i]));
+    int find_set(int i) {
+        return (p[i] == i) ? i : (p[i] = find_set(p[i]));
     }
-    bool isSameSet(int i, int j) {
-        return findSet(i) == findSet(j);
+    bool is_same_set(int i, int j) {
+        return find_set(i) == find_set(j);
     }
-    void unionSet(int i, int j) {
-        if (!isSameSet(i, j)) {
-            --numSets;
-            int x = findSet(i), y = findSet(j);
+    void union_set(int i, int j) {
+        if (!is_same_set(i, j)) {
+            --num_sets;
+            int x = find_set(i), y = find_set(j);
             if (rank[x] > rank[y]) {
                 p[y] = x;
+                sz[x] += sz[y];
             } else {
                 p[x] = y;
+                sz[y] += sz[x];
                 if (rank[x] == rank[y]) {
                     rank[y]++;
                 }
             }
         }
     }
-    int numDisjointSets() {
-        return numSets;
+    int num_disjoint_sets() {
+        return num_sets;
+    }
+    int set_sz(int i) {
+        int x = find_set(i);
+        return sz[x];
     }
 };
