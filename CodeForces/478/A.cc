2dfd1faec917091c098eeaf438692e28c2096503
yassin64b <yassin.bahloul@gmx.de>
2018-05-06 23:08:24 +0200

    CodeForces 478 div2: A, B, C

---
+++ CodeForces/478/A.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class TaskA {
+public:
+    void solve(istream& in, ostream& out) {
+        int n;
+        in >> n;
+        string s;
+        set<set<char>> res;
+        while (in >> s) {
+            set<char> root(begin(s), end(s));
+            res.insert(root);
+        }
+        out << res.size() << "\n";
+    }
+};
+
+int main() {
+    std::ios::sync_with_stdio(false); cin.tie(nullptr);
+    TaskA solver;
+    std::istream& in(std::cin);
+    std::ostream& out(std::cout);
+    solver.solve(in, out);
+    return 0;
+}
