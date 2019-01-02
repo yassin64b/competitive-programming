b8d29f5b5ca6d13b60e08684cf3f4ed095864476
yassin64b <yassin.bahloul@gmx.de>
2019-01-02 14:12:05 +0100

    Library: fix spacing of fast exponentiation

--- LIB/FastExp_ModInv.cc
+++ LIB/FastExp_ModInv.cc
@@ -1,17 +1,16 @@
-constexpr int MOD = 1000*1000*1000 + 7;
-
-int modPow(int a, int b, int c = MOD){
+// get modular inverse for prime MOD with call modPow(a, MOD - 2, MOD)
+int modPow(int a, int b, int c = MOD) {
     long long x = 1, y = a;
-    while (b > 0){
-        if (b%2 == 1){
-            x = (x*y) % c;
+    while (b > 0) {
+        if (b % 2 == 1) {
+            x = (x * y) % c;
         }
-        y = (y*y) % c;
+        y = (y * y) % c;
         b /= 2;
     }
     return x % c;
 }
 
-long long inv(long long a, long long b){
+long long inv(long long a, long long b) {
     return (1 < a) ? b - inv(b % a, a) * b / a : 1;
 }
